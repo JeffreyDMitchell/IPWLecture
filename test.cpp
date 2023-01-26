e63d08f4f24fd21eb1c997d5667a0aba060f932d
Jeffrey Mitchell <Jemi6379@colorado.edu>
2023-01-26 13:14:44 -0700

    actual tests!

--- test.cpp
+++ test.cpp
@@ -7,18 +7,32 @@
 // CHECK - failed test-case will not end testing
 
 TEST_CASE("Join functionality", "[Join]")
-{
-    REQUIRE(1 == 1);
+{   
+    std::string word_arr[] = {"this", "is", "a", "test"};
+    std::vector word_vec(std::begin(word_arr), std::end(word_arr));
+    std::string expectation = "thisglueisglueagluetest";
+
+    REQUIRE(Join(word_vec, "glue") == expectation);
 }
 
 TEST_CASE("OddMask functionality", "[OddMask]")
 {
-    REQUIRE(1 == 1);
+    bool expectation_arr[] = {true, false, true, false, true};
+    int num_set[] = {1,2,3,4,5};
+
+    std::vector expectation(std::begin(expectation_arr), std::end(expectation_arr));
+    std::vector num_vec(std::begin(num_set), std::end(num_set));
+
+    REQUIRE(OddMask(num_vec) == expectation);
 }
 
 TEST_CASE("EvenMask functionality", "[EvenMask]")
 {
-    REQUIRE(1 == 1);
-}
+    bool expectation_arr[] = {false, true, false, true, false};
+    int num_set[] = {1,2,3,4,5};
 
-// cant compile because SIGSTKSZ is messed up :(
+    std::vector expectation(std::begin(expectation_arr), std::end(expectation_arr));
+    std::vector num_vec(std::begin(num_set), std::end(num_set));
+
+    REQUIRE(EvenMask(num_vec) == expectation);
+}
